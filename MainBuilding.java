2154d1cb8fbc732906589ecae3b25185680000c7
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2023-11-13 12:25:40 -0500

    e

---
+++ MainBuilding.java
@@ -0,0 +1,89 @@
+public class MainBuilding {
+    public static void main(String[] args) {
+        ApartmentBuilding apartmentComplex1 = new ApartmentBuilding(100, 2, 2);
+        ApartmentBuilding apartmentComplex2 = new ApartmentBuilding(50, 1, 1);
+
+        Warehouse warehouse1 = new Warehouse(5000);
+        Warehouse warehouse2 = new Warehouse(8000);
+
+        CommercialBuilding commercialBuilding1 = new CommercialBuilding(5);
+        CommercialBuilding commercialBuilding2 = new CommercialBuilding(3);
+
+        Building.compareBuildings(apartmentComplex1, apartmentComplex2);
+        Building.compareBuildings(warehouse1, warehouse2);
+        Building.compareBuildings(commercialBuilding1, commercialBuilding2);
+    }
+}
+
+
+
+
+public abstract class Building {
+    protected int marketValue;
+
+    public abstract int calculateMarketValue();
+
+    public int getMarketValue() {
+        return marketValue;
+    }
+
+    public static void compareBuildings(Building building1, Building building2) {
+        int value1 = building1.calculateMarketValue();
+        int value2 = building2.calculateMarketValue();
+
+        if (value1 > value2) {
+            System.out.println("Building 1 is more valuable.");
+        } else if (value1 < value2) {
+            System.out.println("Building 2 is more valuable.");
+        } else {
+            System.out.println("Both buildings have the same market value.");
+        }
+    }
+}
+
+class ApartmentBuilding extends Building {
+    private int numApartments;
+    private int numBedrooms;
+    private int numBathrooms;
+
+    public ApartmentBuilding(int numApartments, int numBedrooms, int numBathrooms) {
+        this.numApartments = numApartments;
+        this.numBedrooms = numBedrooms;
+        this.numBathrooms = numBathrooms;
+        this.marketValue = calculateMarketValue();
+    }
+
+    @Override
+    public int calculateMarketValue() {
+        return numApartments * (numBedrooms + numBathrooms) * 10000; // Adjust the formula as needed
+    }
+}
+
+class Warehouse extends Building {
+    private int sizeInSquareFeet;
+
+    public Warehouse(int sizeInSquareFeet) {
+        this.sizeInSquareFeet = sizeInSquareFeet;
+        this.marketValue = calculateMarketValue();
+    }
+
+    @Override
+    public int calculateMarketValue() {
+        return sizeInSquareFeet * 50; // Adjust the formula as needed
+    }
+}
+
+class CommercialBuilding extends Building {
+    private int numberOfFloors;
+
+    public CommercialBuilding(int numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
+        this.marketValue = calculateMarketValue();
+    }
+
+    @Override
+    public int calculateMarketValue() {
+        return numberOfFloors * 50000; // Adjust the formula as needed
+    }
+}
+
---
+++ PracticeWithBoxing.java
@@ -0,0 +1,25 @@
+class PracticeWithBoxing {
+  public static void main(String[] args) {
+    Integer s = 21;
+    Integer z = 35;
+    int sum = s + z;
+    System.out.println("the sum of s and z is " + sum);
+    int c[] = new int[5];
+
+    Integer cc[] = new Integer[5];
+
+    Number n[] = new Number[5];
+    n[0] = 34;
+    n[1] = 3.14159;
+    n[2] = 56;
+    n[3] = 1;
+    n[4] = 0.1;
+
+    for (Number x : n)
+      {
+        System.out.println(x);
+      }
+   
+    System.out.println("the number is " + s);
+  }
+}
